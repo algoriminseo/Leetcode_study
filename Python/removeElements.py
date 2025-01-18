5da5fea3f8a58124e93576f0cb8a0719b6e2fd09
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-01-18 15:56:21 +0900

    Add files via upload

---
+++ Python/removeElements.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
+            
+             
+    
+nums = [3,2,2,3]
+sol = Solution()
+val = 3
+print(sol.removeElement(nums, val))
